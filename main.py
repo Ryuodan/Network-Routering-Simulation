86b89070d251bec810c62e9afe083596d6e443af
mayartarek <32437335+mayartarek@users.noreply.github.com>
2019-12-09 12:05:30 +0200

    train

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello world')
+import pygame
+
+width=400
+height=300
+pygame.init()
+screen = pygame.display.set_mode((width,height))
+done = False
+
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+        
+        pygame.display.flip()
