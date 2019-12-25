13d489a4bbe8dcb90288e29cfaaa5039e8cc5120
mayartarek <32437335+mayartarek@users.noreply.github.com>
2019-12-25 12:30:56 +0200

    line

--- itemFactory.py
+++ itemFactory.py
@@ -2,11 +2,13 @@ import pygame
 from router import Router
 import program
 
+
 class ItemFactory:
-    def __init__(self,screen,color,width,height,router_img):
+    def __init__(self,screen,color,width,height,router_img,line_img):
         self.router_image_obj = pygame.image.load(router_img)
         self.background_img = pygame.Surface([width, height])
-        pygame.draw.rect(self.background_img, color, [0, 0, width, height]) 
+        self.line_image_obj = pygame.image.load(line_img)
+        pygame.draw.rect(self.background_img , color, [0, 0, width, height]) 
         self.screen=screen
 
     def button(self,img,pos_x,pos_y,size,item_type,action=None):
@@ -15,12 +17,27 @@ class ItemFactory:
         self.screen.blit(img,(pos_x,pos_y))
         width,height=size
         if pos_x+width > mouse[0] > pos_x and pos_y+height > mouse[1] > pos_y:
+            
             if click[0] == 1 and action != None:
                 print('dost 3lya')
                 obj=action(200,200)
                 if item_type=='Router':
                     program.Program.all_routers.append(obj)
                 program.Program.all_sprites.add(obj)
+    
+    def button_line(self,img,pos_x,pos_y,size,action=None):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        self.screen.blit(img,(pos_x,pos_y))        
+        width,height=size
+        if pos_x+width > mouse[0] > pos_x and pos_y+height > mouse[1] > pos_y:
+            if click[0] == 1 and action != None:
+                #first time clicked should initalize start line point
+                #second time clicked should initalize end line point and make a line
+                print('line')
+            
+    
+    
     
     def create_router(self,pos_x,pos_y):
         router_obj=Router(self.router_image_obj,pos_x,pos_y)
@@ -30,8 +47,9 @@ class ItemFactory:
     #CREATE LINEEEEEEE HEEEEREEEEE
     def create_line(self,pos_x,pos_y):
         pass
-    
+
     def update(self):
         self.screen.blit(self.background_img, (0,0))
         self.button(self.router_image_obj,25,50,self.router_image_obj.get_size(),
             item_type='Router',action=self.create_router)
+        self.button_line(self.line_image_obj,25,150,self.line_image_obj.get_size(),action=self.create_line)
--- program.py
+++ program.py
@@ -1,7 +1,6 @@
 import pygame
 from itemFactory import ItemFactory
 
-
 BLACK=(0,0,0)
 
 class Program:
@@ -14,15 +13,17 @@ class Program:
         self.factory_width=100
         self.factory_height=screen.get_size()[1]
         self.router_img_path='sprites/router50.png'
+        self.line_img_path='sprites/line.png'
         self.itemFactory=ItemFactory(screen=self.screen,color=BLACK,
                         width=self.factory_width,height=self.factory_height,
-                        router_img=self.router_img_path)
+                    router_img=self.router_img_path,line_img=self.line_img_path)
+
         self.routers=[]
 
 
     def update(self):
         self.screen.fill(self.bkcolor)
-        
+       
         self.itemFactory.update()
         self.all_sprites.draw(self.screen)
 
